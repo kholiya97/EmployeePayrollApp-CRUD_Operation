e8c49f7c939cbf96d353529c5b21bd1fc69353f3
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-06-12 23:53:11 +0530

    Added file of UC2_UPDATE_EMP_DETAILS_AND_POPULATE_FORM

--- js/new_home.js
+++ js/new_home.js
@@ -58,3 +58,11 @@ const remove = (node) => {
   document.querySelector(".emp-count").textContent = empPayrollList.length;
   createInnerHtml();
 }
+
+// Update a particular employee details
+const update = (node) => {
+  let empPayrollData = empPayrollList.find(empData => empData._id == node.id);
+  if (!empPayrollData) return;
+  localStorage.setItem('editEmp', JSON.stringify(empPayrollData))
+  window.location.replace(site_properties.add_emp_payroll_page);
+}
